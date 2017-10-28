1c3a19fc18c194882cd5fe36591f5c1728820d8a
davl3232 <davl3232@gmail.com>
2017-10-28 02:41:24 -0500

    Agregado cliente JavaFX con la funcionalidad de fibonacci.

---
+++ RestClient/src/main/java/co/edu/javeriana/ws/rest/model/FibbonaciNumber.java
@@ -0,0 +1,35 @@
+package co.edu.javeriana.webservices.rest;
+
+import java.math.BigInteger;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlValue;
+
+@XmlRootElement
+public class FibbonaciNumber {
+	
+	@XmlAttribute
+	private int index;
+	
+	@XmlValue
+	private BigInteger number;
+	
+	public int getIndex() {
+		return this.index;
+	}
+	
+	public BigInteger getNumber() {
+		return this.number;
+	}
+	
+	public FibbonaciNumber() {
+		this.index = 0;
+		this.number = BigInteger.ZERO;
+	}
+	
+	public FibbonaciNumber(int index, BigInteger number) {
+		this.index = index;
+		this.number = number;
+	}
+}
